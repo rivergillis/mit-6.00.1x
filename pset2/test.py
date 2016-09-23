2f0c4733db1535fab455c3211d1a2cf917a79083
River Gillis <jrivergillis@gmail.com>
2016-09-23 15:36:53 -0500

    Solve pset2 #3

---
+++ pset2/test.py
@@ -0,0 +1,36 @@
+# Finds the minimum monthly payment amount to pay off debt in 12 mo
+# Uses bisection search to find the monthly payment to the cent
+
+def lowestMonthly(balance, annualInterestRate):
+
+    monthlyInterestRate = annualInterestRate / 12.0
+
+    guessPaymentLowerBound = balance / 12.0    
+    guessPaymentUpperBound = (balance * (1 + monthlyInterestRate)**12) / 12.0
+
+    epsilon = 0.05
+    while True:
+
+        testBalance = balance
+        guessMonthlyPayment = round(((guessPaymentLowerBound + guessPaymentUpperBound) / 2.0), 2)
+        for i in range(12):
+            testBalance -= guessMonthlyPayment
+            testBalance += (monthlyInterestRate * testBalance)
+        testBalance = round(testBalance, 2)
+        print("Final balance for " + str(guessMonthlyPayment) + " is " + str(testBalance))
+        if (abs(testBalance) < epsilon) and testBalance <= 0:
+            break
+        print("Upper " + str(guessPaymentUpperBound) + "Lower " + str(guessPaymentLowerBound))
+        print("Equal? " + str(guessPaymentUpperBound == guessPaymentLowerBound))
+        if abs(guessPaymentUpperBound - guessPaymentLowerBound) <= .01:
+            break
+        if testBalance < epsilon:
+            guessPaymentUpperBound = guessMonthlyPayment
+            print("Decreasing upper bound to " + str(guessPaymentUpperBound))
+        elif testBalance > epsilon:
+            guessPaymentLowerBound = guessMonthlyPayment
+            print("Increasing lower bound to " + str(guessPaymentLowerBound))
+
+    print("Lowest Payment: " + str(guessMonthlyPayment))
+
+        
