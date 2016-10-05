4baf36aa1b260466eed16ac6f32de93ae3e9bdaa
River Gillis <jrivergillis@gmail.com>
2016-10-05 12:17:39 -0500

    Implemented hand updates

--- pset4/ps4a.py
+++ pset4/ps4a.py
@@ -147,9 +147,10 @@ def updateHand(hand, word):
     hand: dictionary (string -> int)    
     returns: dictionary (string -> int)
     """
-    # TO DO ... <-- Remove this comment when you code this function
-
-
+    newHand = hand.copy()
+    for letter in word:
+        newHand[letter] -= 1
+    return newHand
 
 #
 # Problem #3: Test word validity
